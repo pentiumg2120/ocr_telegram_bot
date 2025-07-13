7d2bcf680c76c1a44434ef896b82962c36131830
pentiumg2120 <shtfgj@yandex.ru>
2025-07-13 20:42:36 +0300

    getting photo from user

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
+import base64
 import logging
 import asyncio
 import config as cf
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import CommandStart
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums.parse_mode import ParseMode
@@ -18,6 +19,16 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def start(message: types.Message):
     await message.reply("Привет")
+
+
+# Получение фото от пользователя
+@dp.message(F.photo)
+async def get_photo(message: types.Message):
+    photo_id = message.photo[-1].file_id
+    photo = await bot.get_file(photo_id)
+    file = await bot.download_file(photo.file_path)
+    encode_file = base64.b64encode(file.getvalue()).decode('utf-8')
+    print(encode_file)
 
 
 async def main():
