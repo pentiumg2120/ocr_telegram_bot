003b56f4ce0c2f5d15148f3146c8170ad0e0f612
pentiumg2120 <shtfgj@ya.ru>
2025-07-15 23:16:34 +0300

    Added error handling Improved user interaction (added the /help command, text explaining the work of the bot) the promt has been moved to a separate file

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
+import config
+import text
+from cloud import photo_to_text
+
 import logging
 import asyncio
-import config
-
 from aiogram import Bot, Dispatcher, types, F
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums.parse_mode import ParseMode
-
-from cloud import photo_to_text
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -19,17 +19,29 @@ dp = Dispatcher()
 # Стартовое сообщение
 @dp.message(CommandStart())
 async def start(message: types.Message):
-    await message.reply("Привет")
+    await message.reply(text.start)
+
+
+# Ответ на команду /help
+@dp.message(Command("help"))
+async def start(message: types.Message):
+    await message.answer(text.help)
 
 
 # Получение фото от пользователя
 @dp.message(F.photo)
 async def get_photo(message: types.Message):
-    photo_id = message.photo[-1].file_id
-    photo = await bot.get_file(photo_id)
-    file = await bot.download_file(photo.file_path)
-    text_from_photo = await photo_to_text(file.getvalue())
-    await message.reply(text=text_from_photo, parse_mode=None)
+    waiting_message = await message.reply(text=text.waiting, parse_mode=None)
+    try:
+        photo_id = message.photo[-1].file_id
+        photo = await bot.get_file(photo_id)
+        file = await bot.download_file(photo.file_path)
+    except Exception:
+        await waiting_message.edit_text(text=text.download_error, parse_mode=None)
+        logger.error("Ошибка при загрузке фото")
+    else:
+        text_from_photo = await photo_to_text(file.getvalue())
+        await waiting_message.edit_text(text=text_from_photo, parse_mode=None)
 
 
 async def main():
--- cloud.py
+++ cloud.py
@@ -1,4 +1,6 @@
 import config
+import promt, text
+
 from google import genai
 from google.genai import types
 
@@ -7,13 +9,17 @@ client = genai.Client(api_key=config.gemini_api_key)
 
 async def photo_to_text(encoded_image):
     global client
-    image = types.Part.from_bytes(
-        data=encoded_image, mime_type="image/jpeg"
-    )
-    promt = "You are a document processor. Your task is to extract all meaningful text from the provided image, including mathematical calculations and formulas. Identify and discard all irrelevant information, such as system UI elements (e.g., battery life, network signal, and time). Organize the remaining text logically for maximum human readability and present it as a single, clean block of plain text. Do not use any special formatting, markup (like HTML or Markdown), or add any introductory or concluding phrases. Your output must be only the extracted content."
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=[promt, image],
-    )
-    print(response.text)
-    return response.text
+    try:
+        image = types.Part.from_bytes(
+            data=encoded_image, mime_type="image/jpeg"
+        )
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=[promt.photo_to_text, image],
+        )
+
+    except Exception:
+        return text.ai_error
+
+    else:
+        return response.text
